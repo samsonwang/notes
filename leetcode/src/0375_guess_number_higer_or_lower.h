9c5d301d5eb057e87e3bf0c3273c60cd3c347a06
samson <zl.wang@qq.com>
2017-09-07 16:31:50 +0800

    leetcode 0375 dynamic programming

--- leetcode/src/0375_guess_number_higer_or_lower.h
+++ leetcode/src/0375_guess_number_higer_or_lower.h
@@ -25,3 +25,29 @@ public:
 	}
 };
 
+
+
+
+class Solution
+{
+public:
+	int getMoneyAmount(int n)
+	{
+		int** table = new int[n+1][n+1];
+		for (int j = 2; j<=n; j++)
+		{
+			for (int i = j-1; i>0; i--)
+			{
+				int globalMin = 0xffffffff;
+				for (int k = i+1; k<j; k++)
+				{
+					int localMax = k + std::max(table[i][k-1], table[k+1][j]);
+					globalMin = std::min(globalMin, localMax);
+				}
+
+				table[i][j] = i+1==j ? i : globalMin;
+			}
+		}
+		return table[1][n];
+	}
+}
