25f6335bf6d38cea5bc8869a5323377cd2bb2114
samson <zl.wang@qq.com>
2017-09-14 21:41:06 +0800

    leetcode 0371

---
+++ leetcode/src/0371_sum_of_two_integers.h
@@ -0,0 +1,17 @@
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+*/
+
+class Solution
+{
+public:
+	int getSum(int a, int b)
+	{
+
+
+	}
+};
+
