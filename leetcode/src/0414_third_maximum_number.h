953fcff128bbe1557caa6cf8b7b40d1371f29f2a
SamsonWang <zl.wang@foxmail.com>
2017-12-08 09:23:01 +0800

    leetcode 0414

--- leetcode/src/0414_third_maximum_number.h
+++ leetcode/src/0414_third_maximum_number.h
@@ -1,9 +1,14 @@
+
 /*
 Given a non-empty array of integers, return the third maximum number in this array.
 If it does not exist, return the maximum number. The time complexity must be in O(n).
 */
 
-// © for utf-8
+/*
+1. 对于重复的数并不重复计算
+2. 题目中没有指定数组中数的最小值，所以需要自己从数组中找一些数据作为初值
+
+*/
 
 #include <vector>
 
@@ -23,15 +28,17 @@ public:
 			return nums[0] > nums[1] ? nums[0] : nums[1];
 		}
 
-		int nMax1 = -99999;
-		int nMax2 = -99999;
-		int nMax3 = -99999;
+		// 将前三个数中
+		int nMax1 = nums[0] < nums[1] ? nums[0] : nums[1];
+		nMax1 = nMax1 < nums[2] ? nMax1 : nums[2];
+		int nMax2 = nMax1;
+		int nMax3 = nMax1;
 
 		// 3 or more
-		for (size_t i=0; i<nums.size(); ++i) {
+		for (size_t i = 0; i<nums.size(); ++i) {
 			int n = nums[i];
 
-			if (nMax3 > n) {
+			if (nMax3 >= n) {
 				continue;
 			}
 
@@ -40,9 +47,18 @@ public:
 				continue;
 			}
 
+			if (nMax2 == n) {
+				continue;
+			}
+
 			if (nMax1 > n) {
 				nMax3 = nMax2;
 				nMax2 = n;
+				continue;
+			}
+
+			if (nMax1 == n)
+			{
 				continue;
 			}
 
--- leetcode/src/main.cpp
+++ leetcode/src/main.cpp
@@ -2,23 +2,26 @@
 #include "PreCompiled.h"
 
 //#include "0643_maximum_average_subarray_I.h"
-#include "0633_sum_of_square_numbers.h"
+//#include "0633_sum_of_square_numbers.h"
 
 //#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 //#include "catch.hpp"
+
+#include "0414_third_maximum_number.h"
+
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-  Solution_0633 sln;
+  Solution sln;
   
-  // vector<int> nums = {7, 4, 5, 8, 8, 3, 9, 8, 7, 6};
-  auto ret = sln.judgeSquareSum(10);
-  ret = sln.judgeSquareSum(25);
-  ret = sln.judgeSquareSum(100);
+  vector<int> nums = {7, 4, 5, 8, 8, 3, 9, 8, 7, 6};
   
-  
+  auto ret = sln.thirdMax(nums);
+  //ret = sln.judgeSquareSum(25);
+  //ret = sln.judgeSquareSum(100);
+
   getchar();
 
   return 0;
